4307b8aeb07b45ccb5e5886728e62e1fd1e39592
cliveg-clanmil <85290171+cliveg-clanmil@users.noreply.github.com>
2021-06-03 21:27:06 +0100

    Initial Project Creation

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Hub Project..");
+            Console.WriteLine("Code Change 1..");
+            Console.WriteLine("Code Change 2..");
+        }
+    }
+}
